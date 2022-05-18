bebdd0dc8cd868c73995590852f7497c15d1ac88
jack-storment <jstorment1998@gmail.com>
2022-05-18 08:57:25 -0400

    updates

--- src/sections/others/Contact.js
+++ src/sections/others/Contact.js
@@ -39,16 +39,16 @@ const Contact = ({ hero = true, bg = "dark", ...rest }) => {
                 <div>
                   <Text color="light">Email me at</Text>
 
-                  <a href="mailto:hello@folio.com" className="font-weight-bold">
-                    <Span color="primary">hello@folio.com</Span>
+                  <a href="mailto:stormentjd@gmail.com" className="font-weight-bold">
+                    <Span color="primary">stormentjd@gmail.com</Span>
                   </a>
                 </div>
                 <div className="mt-5">
                   <Text color="light">Call me at</Text>
 
                   <div>
-                    <a href="tel:+1-402-4983" className="font-weight-bold">
-                      <Span color="primary">+1-402-4983</Span>
+                    <a href="tel:919-508-4830" className="font-weight-bold">
+                      <Span color="primary">919-508-4830</Span>
                     </a>
                   </div>
                 </div>
