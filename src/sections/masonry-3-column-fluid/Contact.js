fe0b0055d3534c751d26efeed6e0d7b09351066b
jack-storment <jstorment1998@gmail.com>
2022-05-18 09:27:05 -0400

    updates

--- src/sections/masonry-3-column-fluid/Contact.js
+++ src/sections/masonry-3-column-fluid/Contact.js
@@ -14,7 +14,7 @@ const Hero = () => {
             <Col lg="10" xl="7">
               <Availability />
               <div className="text-center my-5">
-                <Title>Do you have illustration project? Let's talk.</Title>
+                <Title>Do you have a project? Let's talk.</Title>
               </div>
               <div className="text-center">
                 <ButtonIcon>Let's Talk Now</ButtonIcon>
