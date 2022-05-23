393d13c95b47d3229745f7a75d8dc5c4c37806c8
jack-storment <jstorment1998@gmail.com>
2022-05-23 17:45:35 -0400

    - Updates

---
+++ .cache/sync-requires.js
@@ -0,0 +1,32 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/.cache/dev-404-page.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/about.js"))),
+  "component---src-pages-agency-1-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/agency-1.js"))),
+  "component---src-pages-agency-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/agency-2.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/contact.js"))),
+  "component---src-pages-designer-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/designer-2.js"))),
+  "component---src-pages-devloper-1-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/devloper-1.js"))),
+  "component---src-pages-devloper-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/devloper-2.js"))),
+  "component---src-pages-grid-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/grid-2.js"))),
+  "component---src-pages-grid-3-fluid-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/grid-3-fluid.js"))),
+  "component---src-pages-grid-3-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/grid-3.js"))),
+  "component---src-pages-grid-4-fluid-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/grid-4-fluid.js"))),
+  "component---src-pages-grid-4-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/grid-4.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/index.js"))),
+  "component---src-pages-masonry-2-column-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/masonry-2-column.js"))),
+  "component---src-pages-masonry-3-column-fluid-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/masonry-3-column-fluid.js"))),
+  "component---src-pages-masonry-3-column-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/masonry-3-column.js"))),
+  "component---src-pages-masonry-4-column-fluid-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/masonry-4-column-fluid.js"))),
+  "component---src-pages-masonry-4-column-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/masonry-4-column.js"))),
+  "component---src-pages-photography-1-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/photography-1.js"))),
+  "component---src-pages-photography-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/photography-2.js"))),
+  "component---src-pages-portfolio-details-2-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/portfolio-details-2.js"))),
+  "component---src-pages-portfolio-details-js": hot(preferDefault(require("/Users/jack/Desktop/Desktop - Jack’s MacBook Pro/absolutefinalportfolio/src/pages/portfolio-details.js")))
+}
+
--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -1,21 +1,14 @@
 export const menuItems = [
   {
     name: "demos",
-    label: "creative demos",
+    label: "React Web Apps",
     items: [
-      { name: "", label: "designer 01" },
-      { name: "designer-2", label: "designer 02" },
-      { name: "devloper-1", label: "devloper 01" },
-      { name: "devloper-2", label: "devloper 02" },
-      { name: "agency-1", label: "agency 01" },
-      { name: "agency-2", label: "agency 02" },
-      { name: "photography-1", label: "photography 01" },
-      { name: "photography-2", label: "photography 02" },
+      { name: "designer-2", label: "portfolio" },
     ],
   },
   {
-    name: "pages",
-    label: "pages",
+    name: "React Native Apps",
+    label: "React Native Apps",
     items: [
       {
         name: "portfolio",
