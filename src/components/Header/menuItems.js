203138c2a7d7d12defd853c4f77d5fd1549cb1a7
jack-storment <jstorment1998@gmail.com>
2022-05-24 13:19:51 -0400

    updates

--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -65,16 +65,6 @@ export const menuItems = [
           },
         ],
       },
-      {
-        name: "portfolio-details",
-        label: "portfolio Details",
-        items: [
-          { name: "portfolio-details", label: "details 01" },
-          { name: "portfolio-details-2", label: "details 02" },
-        ],
-      },
-      { name: "about", label: "about" },
-      { name: "contact", label: "contact" },
     ],
   },
 ];
